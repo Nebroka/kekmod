b68b6e543f9aee45a8e03beb95d524b555ad309c
Nebroka <thecannonlp@gmail.com>
2022-12-04 22:56:21 +0100

    added very important comment and added stuff i think

---
+++ src/main/java/net/nebroka/kekmod/KekMod.java
@@ -0,0 +1,16 @@
+package net.nebroka.kekmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KekMod implements ModInitializer {
+	public static final String MOD_ID = "kekmod";
+	//very important comment
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/nebroka/kekmod/KekModClient.java
@@ -0,0 +1,10 @@
+package net.nebroka.kekmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KekModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
